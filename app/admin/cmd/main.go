c2309138416126573cec2a80f9731e8cc46f8808
王真 <1046060018@qq.com>
2021-10-16 21:30:21 +0800

    chore: 修改启动时描述

--- app/admin/cmd/main.go
+++ app/admin/cmd/main.go
@@ -19,15 +19,16 @@ func main() {
 	http.Init(config.Conf)
 
 	closeFunc := func() {
-		fmt.Printf("%s try to close...\n", config.Conf.Server.Name)
+		fmt.Printf("%s is closing ...\n", config.Conf.Server.Name)
 	}
 
+	fmt.Printf("%s START!\n", config.Conf.Server.Name)
+	defer fmt.Printf("%s EXIT!\n", config.Conf.Server.Name)
+
 	// signal handler
-	fmt.Printf("%s start\n", config.Conf.Server.Name)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
 	s := <-c
-	fmt.Printf("%s get a signal %s\n", config.Conf.Server.Name, s.String())
+	fmt.Printf("%s got signal: %s\n", config.Conf.Server.Name, s.String())
 	closeFunc() // TODO 限制关闭时间
-	fmt.Printf("%s exit\n", config.Conf.Server.Name)
 }
